a0d7a6810a5997b023c984e26ad2cbca12a2331d
atweek <ramille655@gmail.com>
2023-05-21 08:12:08 +0300

    v 0.1

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
+sleep 5
+kubectl apply -f ./confs/nginx/deploy.yaml
+kubectl apply -f ./confs/nginx/nginx_configmap.yaml
+kubectl apply -f ./confs/nginx/nginx-service.yaml
+sleep 60
+kubectl apply -f ./confs/cert-manager
+sleep 10
+kubectl apply -k .
+sleep 60
+echo "[ГОТОВО]"
