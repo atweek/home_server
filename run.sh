71b21fec045151b717055fc2f14b63d38d0d94c6
atweek <ramille655@gmail.com>
2023-06-01 20:00:22 +0300

    fix maria

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
+kubectl create namespace home
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 echo -e "${RED}[CERT-MANAGER]${NC}"
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
 sleep 30
@@ -14,10 +16,14 @@ sleep 60
 echo -e "${RED}[CERT]${NC}"
 kubectl apply -f ./confs/cert-manager
 sleep 10
+echo -e "${RED}[ARGOCD]${NC}"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f confs/argocd/ingress.yaml
+sleep 20
+argocd admin initial-password -n argocd > confs/other/argopass
+sleep 10
 echo -e "${RED}[OTHER]${NC}"
 kubectl apply -k .
 sleep 60
-
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo -e "${RED}[ГОТОВО]${NC}"
